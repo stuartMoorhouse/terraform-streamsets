1295b122b1538b6278aee87d827c0a52cd021846
Stuart Moorhouse <Stuart.Moorhouse@marklogic.com>
2020-03-11 22:50:35 +0000

    changing Admin's password with the admin_password variable

--- stream-sets.tf
+++ stream-sets.tf
@@ -22,14 +22,14 @@ resource "digitalocean_droplet" "streamsets" {
 
   provisioner "remote-exec" {
     inline = [
-      # run StreamSets from a Docker image
+      # pull the StreamSets image from DockerHub and run it
       "docker volume create --name sdc-data",
       "docker pull streamsets/datacollector",
       "docker run --restart on-failure -p 18630:18630 -d -v sdc-data:/data --name streamsets-dc streamsets/datacollector dc",
     
-      # TO DO: change the admin user's default password
-     # " docker exec -it streamsets-dc /bin/bash -c \"password=$(echo -n PASSWORD | sudo md5sum | cut -d' ' -f1) && sed -i \"s/admin.*/admin:   MD5:$password,user,admin/\" /etc/sdc/form-realm.properties\" ",
-     # "docker restart streamsets-dc"
+      # change the Admin user's password, by creating a hash of the value of ${var.admin_password} and replacing the hash of the default Admin password (found in /etc/sdc/form-realm.properties) with it
+     " docker exec -it streamsets-dc /bin/bash -c 'password=$(echo -n ${var.admin_password} | sudo md5sum | cut -d\" \" -f1) && sed -i \"s/admin.*/admin:   MD5:$password,user,admin/\" /etc/sdc/form-realm.properties' ",
+     "docker restart streamsets-dc"
     ]
   }
 
